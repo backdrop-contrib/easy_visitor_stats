88d78f4ee228ed59eb2391d975a001fb30ab533e
indigoxela <user@olga.indigofloat.at>
2026-06-19 16:33:29 +0200

    Initial commit

---
+++ js/easy-visitor-stats.js
@@ -0,0 +1,41 @@
+(function () {
+  "use strict";
+
+  // Initial value.
+  document.addEventListener('DOMContentLoaded', function() {
+    Backdrop.settings.easyVisitorStart = new Date().getTime();
+  });
+
+  document.addEventListener('visibilitychange', function() {
+    // Moved away from this browser tab, then comes back - reset timer. Moving
+    // away already triggered sendBeacon().
+    if (document.visibilityState === 'visible') {
+      Backdrop.settings.easyVisitorStart = new Date().getTime();
+    }
+
+    // Move away or close tab or close browser.
+    if (document.visibilityState === 'hidden') {
+      const now = new Date().getTime();
+      let startTime = now;
+      if (Backdrop.settings.easyVisitorStart !== undefined) {
+        startTime = Backdrop.settings.easyVisitorStart;
+      }
+      const duration = (now - startTime);
+      const data = {
+        href: window.location.href,
+        duration: duration,
+        timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,// is this useful, anyway?
+        referrer: document.referrer,
+        platform: navigator.platform,
+        ua: navigator.userAgent
+      };
+      // In order to let those show up in POST for PHP it has to be FormData.
+      let formData = new FormData();
+      for (const property in data) {
+        formData.append(property, data[property]);
+      }
+      // @todo url from conf! base_path! or base url and hardcode?
+      navigator.sendBeacon("/easy-visitor-stats", formData);
+    }
+  });
+})();
