f9dd4e082ac09666b32df594a93a830ffe0df061
indigoxela <user@olga.indigofloat.at>
2026-06-24 14:45:35 +0200

    Dynamic history graph

--- js/easy-visitor-charts.js
+++ js/easy-visitor-charts.js
@@ -1,15 +1,44 @@
+/**
+ * @file
+ * Draw various charts on the reports dashboard.
+ */
 (function ($) {
   "use strict";
   Backdrop.behaviors.easyVisitorCharts = {
-
+    /**
+     * Dynamically fetch charts data.
+     *
+     * @param object chartObj
+     * @param string fetchUrl
+     * @param string title
+     */
+    updateChart: function (chartObj, fetchUrl, title) {
+      fetch(fetchUrl)
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(response) {
+          chartObj.data.labels = response.labels;
+          chartObj.data.datasets[0].data = response.values;
+          chartObj.options.plugins.title.text = title;
+          // Add some padding above.
+          chartObj.options.scales.y.suggestedMax = Math.max(...response.values) + 1;
+          chartObj.update();
+        });
+    },
+    /**
+     * {@inheritdoc}
+     */
     attach: function (context, settings) {
-      const historyChartElement = document.getElementById('easy-visitor-history-chart');
-      // Inherit color from parent. @todo find a better element to pick color.
-      const styles = getComputedStyle(historyChartElement);
+      // Inherit color from content for better readability.
+      const styles = getComputedStyle(document.querySelector('.easy-visitor-boxes'));
       Chart.defaults.color = styles.color;
-      Chart.defaults.scale.ticks.autoSkipPadding = 10;
+      Chart.defaults.scale.ticks.autoSkipPadding = 15;
+      Chart.defaults.elements.point.hitRadius = 10;
+      Chart.defaults.elements.line.tension = 0.2;
 
       const hitsChartElement = document.getElementById('easy-visitor-hits-chart');
+      const hitsData = settings.easyVisitorStats.hitsData;
       const hitsChart = new Chart(hitsChartElement, {
         type: 'line',
         options: {
@@ -17,6 +46,7 @@
           scales: {
             y: {
               min: 0,
+              suggestedMax: Math.max(...hitsData.datasets[0].data) + 1,
             },
           },
           plugins: {
@@ -26,18 +56,14 @@
           },
           elements: {
             line: {
-              tension : 0.3,
               fill: 'origin',
-            },
-            point: {
-              radius: 3,
-              hitRadius: 10,
             },
           },
         },
-        data: settings.easyVisitorStats.hitsData,
+        data: hitsData,
       });
 
+      const historyChartElement = document.getElementById('easy-visitor-history-chart');
       const historyChart = new Chart(historyChartElement, {
         type: 'line',
         options: {
@@ -51,9 +77,28 @@
             legend: {
               display: false,
             },
+            title: {
+              display: true,
+              text: '',
+            }
           },
         },
         data: settings.easyVisitorStats.historyData,
+      });
+
+      // Fetch dynamically.
+      const fetchUrlBase = settings.easyVisitorStats.historyDataFetchBase;
+      const toggleElement = document.getElementById('history-graph-date-toggle');
+      const initialValues = toggleElement.value.split(',');
+      let fetchUrl = fetchUrlBase + initialValues[0] + '/' + initialValues[1];
+      let title = toggleElement.options[toggleElement.selectedIndex].text;
+      Backdrop.behaviors.easyVisitorCharts.updateChart(historyChart, fetchUrl, title);
+
+      toggleElement.addEventListener('change', function (event) {
+        let newTitle = event.target.options[event.target.selectedIndex].text;
+        let newValues = event.target.value.split(',');
+        let newFetchUrl = fetchUrlBase + newValues[0] + '/' + newValues[1];
+        Backdrop.behaviors.easyVisitorCharts.updateChart(historyChart, newFetchUrl, newTitle);
       });
 
       const pieChartOptions = {
